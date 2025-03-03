9910806b89a7aef721165dacb0caead9753066e1
Mohsin <mdmohsinhaider6@gmail.com>
2025-03-04 02:07:38 +0530

    Admin setup

---
+++ src/main/java/com/jspider/hospital_management_system_spring_boot/controller/AdminController.java
@@ -0,0 +1,22 @@
+package com.jspider.hospital_management_system_spring_boot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jspider.hospital_management_system_spring_boot.service.AdminService;
+
+@RestController
+@RequestMapping(value = "/api/authenticateAdmin")
+public class AdminController {
+
+	@Autowired
+	private AdminService service;
+	
+	@PostMapping(value = "/identifier/{identifier}/password/{password}")
+	public boolean authenticateAdminController(@PathVariable String identifier, @PathVariable String password) {
+		return service.authenticateAdmin(identifier, password);
+	}
+}
---
+++ src/main/java/com/jspider/hospital_management_system_spring_boot/entity/AdminImpl.java
@@ -0,0 +1,35 @@
+package com.jspider.hospital_management_system_spring_boot.entity;
+
+import org.springframework.stereotype.Repository;
+
+import com.jspider.hospital_management_system_spring_boot.dao.AdminDao;
+import com.jspider.hospital_management_system_spring_boot.reposetory.AdminRepository;
+
+@Repository
+public class AdminImpl implements AdminDao {
+	
+	private AdminRepository repository;
+	
+	/**
+	 * @param repository
+	 */
+	public AdminImpl(AdminRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	public boolean getAdminByIdAndPassword(int adminId, String password) {
+		return repository.existsByIdAndPassword(adminId, password);
+	}
+
+	@Override
+	public boolean getAdminByEmailAndPassword(String email, String password) {
+		return repository.existsByEmailAndPassword(email, password);
+	}
+	
+	
+    
+
+	
+}
